c2028c8ebc09e3ea34446f30128845fec235ed1e
Kaio Pomini <kaiopomini@gmail.com>
2019-06-06 22:14:41 -0300

    1st
    1st

---
+++ aula11/src/aula11/Program.java
@@ -0,0 +1,28 @@
+package aula11;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		
+		boolean completed = false;
+		char gender = 'F'; // pode usar o código unicode exemplo \u0041 para o caractere 'A'
+		byte n1 = 126;
+		int n2 = 1000;
+		int n3 = 2147483647;
+		long n4 = 1234567890123456L;
+		float n5 = 1.552f;
+		double n6 = 1.666262484654654;		
+		
+		System.out.println(completed);
+		System.out.println(gender);
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		System.out.println(n5);
+		System.out.println(n6);
+		
+	}
+
+}
